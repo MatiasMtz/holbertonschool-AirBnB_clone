222cfb77b4a46ce99f1dd622419325dba8dddd54
MatiasMtz <4442@holbertonschool.com>
2022-06-27 16:57:15 +0000

    task 4 finished

--- mat/base_model.py
+++ mat/base_model.py
@@ -7,43 +7,50 @@ from datetime import datetime
 
 
 class BaseModel():
-    """
+	"""
 
-    """
+	"""
 
-    def __init__(self):
-        """
-        
-        """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+	def __init__(self, *args, **kwargs):
+		"""
 
-    def __str__(self):
-        """
+		"""
+		if kwargs is not None and len(kwargs) is not 0:
+			for key, value in kwargs.items():
+				if key != "__class__":
+					if key == "created_at" or key == "updated_at":
+						value = datetime.fromisoformat(value)
+					setattr(self, key, value)
+		else:
+			self.id = str(uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
 
-        """
-	    
-        class_name = type(self).__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+	def __str__(self):
+		"""
 
-    def save(self):
-        """
+		"""
 
-        """
+		class_name = type(self).__name__
+		return f"[{class_name}] ({self.id}) {self.__dict__}"
 
-        self.updated_at = datetime.now()
+	def save(self):
+		"""
 
-    def to_dict(self):
-        """
-        
-        """
+		"""
 
-        to_dict = {}
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                value = value.isoformat()
-            to_dict[key] = value
-        to_dict["__class__"] = type(self).__name__
+		self.updated_at = datetime.now()
 
-        return (to_dict)
+	def to_dict(self):
+		"""
+
+		"""
+
+		to_dict = {}
+		for key, value in self.__dict__.items():
+			if key == "created_at" or key == "updated_at":
+				value = value.isoformat()
+			to_dict[key] = value
+		to_dict["__class__"] = type(self).__name__
+
+		return (to_dict)
