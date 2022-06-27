f69a0d737c6661680edc04b87ff192c561c0f444
MatiasMtz <4442@holbertonschool.com>
2022-06-27 15:16:47 +0000

    main workspace updated

---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+
+"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel():
+    """
+
+    """
+
+    def __init__(self):
+        """
+        
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+
+        """
+	    
+        class_name = type(self).__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        
+        """
+
+        to_dict = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                value = value.isoformat()
+            to_dict[key] = value
+        to_dict["__class__"] = type(self).__name__
+
+        return (to_dict)
