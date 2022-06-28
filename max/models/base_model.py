530113c934e8462c64cbde86a8045b22fe4f7696
MaxiHBTN <4387@holbertonschool.com>
2022-06-28 16:54:16 +0000

    Task 5

--- max/models/base_model.py
+++ max/models/base_model.py
@@ -4,7 +4,7 @@
 """
 from uuid import uuid4
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel():
@@ -26,7 +26,7 @@ class BaseModel():
 			self.id = str(uuid4())
 			self.created_at = datetime.now()
 			self.updated_at = datetime.now()
-			storage.new(self)
+			models.storage.new(self)
 
 	def __str__(self):
 		"""
@@ -42,7 +42,7 @@ class BaseModel():
 		"""
 
 		self.updated_at = datetime.now()
-		storage.save()
+		models.storage.save()
 
 	def to_dict(self):
 		"""
--- max/models/engine/file_storage.py
+++ max/models/engine/file_storage.py
@@ -4,46 +4,50 @@
 """
 
 import json
-
+import os
+from models.base_model import BaseModel
 
 class FileStorage:
-	"""
-	
-	"""
-	__file_path = "file.json"
-	__objects = {}
-	def all(self):
-		"""
-		
-		"""
-		return FileStorage.__objects
-	
-	def new(self, obj):
-		"""
-		
-		"""
-		key = f"{obj.__class__.__name__}.{obj.id}"
-		FileStorage.__objects[key] = obj
+    """
+    
+    """
+    __file_path = "file.json"
+    __objects = {}
+    def all(self):
+        """
+        
+        """
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """
+        
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
 
-	def save(self):
-		"""
+    def save(self):
+        """
 
-		"""
-		obj_dict = {}
-		for key, value in FileStorage.__objects.items():
-			obj_dict[key] = value.to_dict()
-		with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-			json.dump(obj_dict, file)
-	
-	def reload(self):
-		"""
-		
-		"""
-		try:
-			with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-				dict_obj = json.load(file)
-				for key, value in dict_obj.items():
-					FileStorage.__objects[key] = eval(value["__class__"])(**value)
-		except Exception:
-			pass
-		
+        """
+        obj_dict = {}
+        for key, value in FileStorage.__objects.items():
+            obj_dict[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
+    
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                dict_obj = json.load(file)
+                for key, value in dict_obj.items():
+                    FileStorage.__objects[key] = eval(value["__class__"])(**value)
+        except Exception:
+            pass
+        """
+        if os.path.isfile(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r+', encoding='utf-8') as f:
+                obj = json.load(f)
+                for key, value in obj.items():
+                    FileStorage.__objects[key] = eval(value['__class__'])(**value)
+        """
