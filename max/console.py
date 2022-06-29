0d6c7c9770cd5854258ee5a0a00cb322c6900ebf
MaxiHBTN <4387@holbertonschool.com>
2022-06-29 21:14:31 +0000

    Advanced Tasks

--- max/console.py
+++ max/console.py
@@ -8,6 +8,14 @@ import sys
 import shlex
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+classes = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -47,10 +55,10 @@ class HBNBCommand(cmd.Cmd):
 
         if cls_name is None:
             print ("** class name missing **")
-        elif cls_name != "BaseModel":
+        elif cls_name not in classes:
             print ("** class doesn't exist **")
         else:
-            new = BaseModel()
+            new = eval(cls_name)()
             new.save()
             print (new.id)
 
@@ -61,14 +69,14 @@ class HBNBCommand(cmd.Cmd):
             key = args[0] + "." + args[1]
         if len(args) == 0:
             print ("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in classes:
             print ("** class doesn't exist **")
         elif len(args) == 1:
             print ("** instance id missing **")
         elif key not in storage.all():
             print ("** no instance found **")
         else:
-            print (storage.all()[key])
+            print(storage.all()[key])
 
     def do_destroy(self, arg):
         
@@ -77,7 +85,7 @@ class HBNBCommand(cmd.Cmd):
             key = args[0] + "." + args[1]
         if len(args) == 0:
             print ("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in classes:
             print ("** class doesn't exist **")
         elif len(args) == 1:
             print ("** instance id missing **")
@@ -93,9 +101,15 @@ class HBNBCommand(cmd.Cmd):
         
         args = shlex.split(arg)
         if args:
-            if args[0] != "BaseModel":
+            if args[0] not in classes:
                 print ("** class doesn't exist **")
-                return
+            inst_list = []
+            for key in storage.all():
+                cls = key.split(".")[0]
+                if cls == args[0]:
+                    inst_list.append(str(storage.all()[key]))
+            print(inst_list)
+            return
         inst_list = []
         for key in storage.all():
             inst_list.append(str(storage.all()[key]))
@@ -110,7 +124,7 @@ class HBNBCommand(cmd.Cmd):
             key = args[0] + "." + args[1]
         if len(args) == 0:
             print ("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in classes:
             print ("** class doesn't exist **")
         elif len(args) == 1:
             print ("** instance id missing **")
@@ -122,6 +136,36 @@ class HBNBCommand(cmd.Cmd):
             print ("** value missing **")
         setattr(storage.all()[key], args[2], args[3])
         storage.all()[key].save()
+    
+    def default(self, arg):
+        count = 0
+        try:
+            args = arg.split(".")
+            cls_name = args[0]
+            command = args[1]
+            if command == "all()":
+                self.do_all(cls_name)
+                return
+            elif command == "count()":
+                for instances in storage.all():
+                    if instances.split(".")[0] == cls_name:
+                        count += 1
+                print(count)
+                return
+            cmmd = command.split("(")[0]
+            id = command.split("(")[1]
+            if cmmd == "show" and id[-1] == ")":
+                id = id[:-1]
+                showArg = f"{cls_name} {id}"
+                self.do_show(showArg)
+                return
+            if cmmd == "destroy" and id[-1] == ")":
+                id = id[:-1]
+                showArg = f"{cls_name} {id}"
+                self.do_destroy(showArg)
+        except Exception:
+            pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
