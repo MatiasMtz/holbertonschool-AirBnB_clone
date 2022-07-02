bcec39b4dfca5029a0463f07676e7adc7e816a65
MaxiHBTN <4387@holbertonschool.com>
2022-07-02 22:19:45 +0000

    tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Unittest for FileStorage class
+"""
+
+import unittest
+from models import storage
+import models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class FileStorage(unittest.TestCase):
+    """
+    Unittest for FileStorage clas
+    """
+    
+    def test_docstring(self):
+        """
+        Test if the class has a docstring
+        """
+        self.assertIsNotNone(FileStorage.__doc__)
+
+    def test_file_storage(self):
+        """
+        Test the instanitation of a new instance
+        of User and its methods
+        """
+
+        file_storage = FileStorage()
+        self.assertEqual(isinstance(file_storage, FileStorage), True)
+
+        self.assertIsNotNone(models.engine.file_storage.FileStorage().all)
+        self.assertIsNotNone(models.engine.file_storage.FileStorage().new)
+        self.assertIsNotNone(models.engine.file_storage.FileStorage().save)
+        self.assertIsNotNone(models.engine.file_storage.FileStorage().reload)
+        self.assertIsNotNone(models.engine.file_storage.FileStorage().all())
+        self.assertIsNotNone(models.storage.all())
+
+if __name__ == '__main__':
+    unittest.main()
