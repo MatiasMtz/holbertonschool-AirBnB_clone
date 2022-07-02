a2f75e8b9dbc6ef3a636bd70045554b7c67034b2
MaxiHBTN <4387@holbertonschool.com>
2022-07-02 20:34:29 +0000

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -32,6 +32,8 @@ class test_amenity(unittest.TestCase):
         """
 
         amenity = Amenity()
+        update = amenity.updated_at
+
         self.assertEqual(isinstance(amenity, Amenity), True)
         self.assertEqual(issubclass(amenity.__class__, BaseModel), True)
 
@@ -39,14 +41,29 @@ class test_amenity(unittest.TestCase):
         self.assertEqual(type(amenity.name), str)
         self.assertEqual(amenity.name, "")
 
-
-        
         """
         str method
         """
 
         string = f"[Amenity] ({amenity.id}) {amenity.__dict__}"
         self.assertEqual(str(amenity), string)
+
+        """
+        save method
+        """
+
+        self.assertEqual(update, amenity.updated_at)
+        amenity.save()
+        self.assertNotEqual(update, amenity.updated_at)
+
+        """
+        to_dict method
+        """
+
+        test_dict = amenity.to_dict()
+        self.assertEqual(type(test_dict), dict)
+        self.assertEqual(type(test_dict["created_at"]), str)
+        self.assertEqual(type(test_dict["updated_at"]), str)
         
 
 if __name__ == '__main__':
