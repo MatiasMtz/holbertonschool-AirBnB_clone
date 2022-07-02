e68ecb2cd48261b1484f0abe3a7d97c5fbc6c4f1
MaxiHBTN <4387@holbertonschool.com>
2022-07-02 20:29:42 +0000

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,14 +1,54 @@
 #!/usr/bin/python3
-"""Amenity Test"""
+"""
+Unittest for Amenity class
+"""
+
 import unittest
-from models.base_model import BaseModel
+from datetime import datetime
+from models.amenity import BaseModel
 from models.amenity import Amenity
 
 
-class TestAmenity(unittest.TestCase):
-    """Test the Amenity module"""
+class test_amenity(unittest.TestCase):
+    """
+    Unittest for Amenity class
+    """
 
-    def test_class(self):
-        """Test the amenity class"""
-        self.assertEqual(Amenity.name, "")
-        self.assertTrue(issubclass(Amenity, BaseModel))
+    def test_docstring(self):
+        """
+        Test if the clas has a docstring
+        """
+
+        self.assertIsNotNone(Amenity.__doc__)
+
+    def test_amenity(self):
+        """
+        Test the instanitation of a new instance
+        of Amenity and its methods
+        """
+        
+        """
+        Instanitation
+        """
+
+        amenity = Amenity()
+        self.assertEqual(isinstance(amenity, Amenity), True)
+        self.assertEqual(issubclass(amenity.__class__, BaseModel), True)
+
+        self.assertIsNotNone(amenity.name)
+        self.assertEqual(type(amenity.name), str)
+        self.assertEqual(amenity.name, "")
+
+
+        
+        """
+        str method
+        """
+
+        string = f"[Amenity] ({amenity.id}) {amenity.__dict__}"
+        self.assertEqual(str(amenity), string)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+    
