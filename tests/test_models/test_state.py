4b6c13bc4a5811ad5f51a15614df1e91d5c82456
MaxiHBTN <4387@holbertonschool.com>
2022-07-02 21:55:25 +0000

    tests

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Unittest for State class
+"""
+
+import unittest
+from datetime import datetime
+from models import state
+from models.base_model import BaseModel
+from models.state import State
+
+
+class test_state(unittest.TestCase):
+    """
+    Unittest for State class
+    """
+
+    def test_docstring(self):
+        """
+        Test if the class has a docstring
+        """
+
+        self.assertIsNotNone(State.__doc__)
+
+    def test_state(self):
+        """
+        Test the instanitation of a new instance
+        of State and its methods
+        """
+        
+        """
+        Instanitation
+        """
+
+        state = State()
+        update = state.updated_at
+
+        self.assertEqual(isinstance(state, State), True)
+        self.assertEqual(issubclass(state.__class__, BaseModel), True)
+
+        self.assertIsNotNone(state.name)
+        self.assertEqual(type(state.name), str)
+        self.assertEqual(state.name, "")
+
+        """
+        str method
+        """
+
+        string = f"[State] ({state.id}) {state.__dict__}"
+        self.assertEqual(str(state), string)
+
+        """
+        save method
+        """
+
+        self.assertEqual(update, state.updated_at)
+        state.save()
+        self.assertNotEqual(update, state.updated_at)
+
+        """
+        to_dict method
+        """
+
+        test_dict = state.to_dict()
+        self.assertEqual(type(test_dict), dict)
+        self.assertEqual(type(test_dict["created_at"]), str)
+        self.assertEqual(type(test_dict["updated_at"]), str)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+    
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+Unittest for User class
+"""
+
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+
+
+class test_user(unittest.TestCase):
+    """
+    Unittest for User class
+    """
+
+    def test_docstring(self):
+        """
+        Test if the class has a docstring
+        """
+
+        self.assertIsNotNone(User.__doc__)
+
+    def test_review(self):
+        """
+        Test the instanitation of a new instance
+        of User and its methods
+        """
+        
+        """
+        Instanitation
+        """
+
+        user = User()
+        update = user.updated_at
+
+        self.assertEqual(isinstance(user, User), True)
+        self.assertEqual(issubclass(user.__class__, BaseModel), True)
+
+        self.assertIsNotNone(user.email)
+        self.assertEqual(type(user.email), str)
+        self.assertEqual(user.email, "")
+        self.assertIsNotNone(user.password)
+        self.assertEqual(type(user.password), str)
+        self.assertEqual(user.password, "")
+        self.assertIsNotNone(user.first_name)
+        self.assertEqual(type(user.first_name), str)
+        self.assertEqual(user.first_name, "")
+        self.assertIsNotNone(user.last_name)
+        self.assertEqual(type(user.last_name), str)
+        self.assertEqual(user.last_name, "")
+
+        """
+        str method
+        """
+
+        string = f"[User] ({user.id}) {user.__dict__}"
+        self.assertEqual(str(user), string)
+
+        """
+        save method
+        """
+
+        self.assertEqual(update, user.updated_at)
+        user.save()
+        self.assertNotEqual(update, user.updated_at)
+
+        """
+        to_dict method
+        """
+
+        test_dict = user.to_dict()
+        self.assertEqual(type(test_dict), dict)
+        self.assertEqual(type(test_dict["created_at"]), str)
+        self.assertEqual(type(test_dict["updated_at"]), str)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+    
