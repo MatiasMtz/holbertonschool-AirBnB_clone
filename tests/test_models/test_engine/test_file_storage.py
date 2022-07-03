51d64af8bcd3b900772cba6944abc292f0179ca4
MaxiHBTN <4387@holbertonschool.com>
2022-07-03 23:20:18 +0000

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,6 +8,12 @@ import unittest
 from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 F_storage = FileStorage()
 B_model = BaseModel()
@@ -65,8 +71,26 @@ class TestFileStorage(unittest.TestCase):
         """ Save  """
         obj = BaseModel()
         obj_key = 'BaseModel' + '.' + obj.id
-        
+        obj1 = User()
+        obj1_key = 'User' + '.' + obj1.id
+        obj2 = City()
+        obj2_key = 'City' + '.' + obj2.id
+        obj3 = Amenity()
+        obj3_key = 'Amenity' + '.' + obj3.id
+        obj4 = Place()
+        obj4_key = 'Place' + '.' + obj4.id
+        obj5 = Review()
+        obj5_key = 'Review' + '.' + obj5.id
+        obj6 = State()
+        obj6_key = 'State' + '.' + obj6.id
+
         self.assertEqual(obj, storage.all()[obj_key])
+        self.assertEqual(obj1, storage.all()[obj1_key])
+        self.assertEqual(obj2, storage.all()[obj2_key])
+        self.assertEqual(obj3, storage.all()[obj3_key])
+        self.assertEqual(obj4, storage.all()[obj4_key])
+        self.assertEqual(obj5, storage.all()[obj5_key])
+        self.assertEqual(obj6, storage.all()[obj6_key])
        
 
 if __name__ == '__main__':
